b494b4c3939811b9a698666eede9efabc1d782cb
José Abadía <jr.avalug@gmail.com>
2024-04-16 01:53:58 -0400

    add buttons for control window

--- app/ui/titlebar.tsx
+++ app/ui/titlebar.tsx
@@ -1,37 +1,64 @@
-import { ComponentProps } from "react";
-import { SlLayers } from "react-icons/sl";
-import { twMerge } from "tailwind-merge";
+import { SlLayers } from 'react-icons/sl'
+import { join } from 'path'
+import { VscChromeClose, VscChromeMaximize, VscChromeMinimize } from 'react-icons/vsc'
 
 export const ContentLeft = () => {
   return (
     <div className="flex items-center h-full grow-[2] justify-start order-none w-[20%] z-[2500]">
-      <span className="pl-6">SAMAG</span>
+      <span className="pl-4">SEBookkeeping</span>
     </div>
-  );
-};
+  )
+}
+
+
+
+
 
 export const ContentCenter = () => {
   return (
     <div className="flex items-center h-full justify-center mx-2 max-w-fit min-w-0 order-1 w-[60%] z-[2500]"></div>
-  );
-};
+  )
+}
 
 export const ContentRight = () => {
   return (
     <div className="flex items-center h-full grow-[2] justify-end min-w-min order-2 w-[20%]">
       <div className="flex justify-end flex-grow-0 flex-shrink-0 relative text-center z-[2500] h-full ml-auto min-w-7">
-        <button className="hover:bg-slate-600/50 px-[18px]" title="Settings">
+        <button
+          className="hover:bg-slate-600/50 px-[18px]"
+          title="Settings"
+        >
           <SlLayers />
         </button>
+        <button
+          className="hover:bg-slate-600/50 px-[18px]"
+          title="Minimize"
+        >
+          <VscChromeMaximize />
+        </button>
+        <button
+          className="hover:bg-slate-600/50 px-[18px]"
+          title="Maximize"
+        >
+          <VscChromeMinimize />
+        </button>
+        <button
+          className="hover:bg-red-600/80 px-[18px]"
+          title="Close"
+        >
+          <VscChromeClose />
+        </button>
       </div>
-      <div className="flex flex-grow-0 flex-shrink-0 text-center z-[3000] h-full w-[138px]"></div>
     </div>
-  );
-};
+  )
+}
 
 const TitleBar = ({ ...props }) => {
   return (
-    <header className={`absolute top-0 h-10 w-full`} {...props}>
+    <header
+      className={`absolute top-0 h-10 w-full bg-slate-900/20`}
+      {...props}
+    >
       <div className="h-10 flex flex-row text-sky-200/70 text-sm overflow-hidden">
         <div className="flex items-center flex-grow flex-shrink w-full h-full overflow-hidden select-none justify-start">
           <div id="titlebar-draggable"></div>
@@ -41,7 +68,7 @@ const TitleBar = ({ ...props }) => {
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default TitleBar;
+export default TitleBar

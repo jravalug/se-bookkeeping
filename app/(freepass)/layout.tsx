28e6787d9a35fca979a602d31908b45a558bc563
jravalug <jr.avalug@gmail.com>
2024-04-24 07:13:23 -0400

    remove container

--- app/(freepass)/layout.tsx
+++ app/(freepass)/layout.tsx
@@ -10,7 +10,7 @@ export default function FreePassLayout({ children }: { children: React.ReactNode
   const heroSrc = '/images/hero' + (resolvedTheme !== 'light' ? '-dark' : '') + heroExt
 
   return (
-    <main className="absolute flex h-[calc(100dvh-40px)] items-center justify-center md:p-24 container top-10">
+    <main className="absolute w-screen flex h-[calc(100dvh-40px)] items-center justify-center md:p-24 top-10">
       <Card className="flex grow flex-col gap-4 md:flex-row rounded-lg border px-6 py-10 md:px-10 max-w-4xl bg-card shadow">
         <div className="flex flex-col justify-center gap-5 md:w-1/2 ">{children}</div>
         <div className="flex items-center justify-center md:w-1/2">

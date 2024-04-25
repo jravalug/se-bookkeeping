283a2ef0a58504f9678424d237ec3769c21dfb9e
José Abadía <jr.avalug@gmail.com>
2024-04-25 04:12:00 -0400

    delete unused data

--- prisma/datas/incomeData.ts
+++
@@ -1,5 +0,0 @@
-export const incomeType = [
-  { name: 'INGRESOS OBTENIDOS' },
-  { name: 'NO CONSIDERADOS A EFECTOS DE IMPUESTOS' }
-]
-export const incomeActivity = [{ name: 'VENTAS' }, { name: 'SERVICIOS' }]
--- prisma/datas/paymentData.ts
+++
@@ -1,2 +0,0 @@
-export const paymentType = [{ name: 'EFECTIVO EN CAJA' }, { name: 'EFECTIVO EN BANCO' }]
-export const paymentMethod = [{ name: 'ELECTRÓNICO' }, { name: 'EN BANCO' }]
